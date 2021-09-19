1ff4d74a169627079f80ed07d5246e34f78c4c5f
HikaruEgashira <s1811320@gmail.com>
2021-09-20 08:20:34 +0900

    add description

--- example/index.tsx
+++ example/index.tsx
@@ -5,12 +5,23 @@ const Link: React.FC<{ href: string }> = ({ href, children }) => (
     <a href={href}>{children}</a>
 );
 
+// alternative syntax
 const Link1: FCX<"a"> = ({ children, ...props }) => (
     <a href={props.href}>{children}</a>
 );
 
-const Link2 = ({ children, ...props }: PX<typeof Link>) => (
-    <Link {...props}>{children}</Link>
+// extends functional component
+const Link2 = ({ children, ...props }: PX<typeof Link1>) => (
+    <Link1 {...props}>{children}</Link1>
 );
 
+// using library
 const Pdf: FCX<Document> = (props) => <Document {...props} />;
+
+const Main = () => (
+    <>
+        <Link2 className="text-xl" style={{ opacity: 80 }} href="#hello">
+            Hello
+        </Link2>
+    </>
+);
